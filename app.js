eb50660318b382a32cf13a93dfc9f06177edb0ba
rayGreycloud <rayGreycloud@gmail.com>
2017-02-08 11:31:12 -0800

    Add error handling

--- app.js
+++ app.js
@@ -12,4 +12,9 @@ if (process.env.NODE_ENV !== 'test') {
 app.use(bodyParser.json());
 routes(app);
 
+app.use((err, req, res, next) => {
+  // on error send status code 422 Unprocessable Entity
+  res.status(422).send({ error: err.message });
+});
+
 module.exports = app;
